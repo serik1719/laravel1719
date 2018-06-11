e6caef6d63fb7a922f17b47dbdc8f81e633b3be5
serik1719 <serik1719@mail.ru>
2018-06-11 23:01:40 +0300

    all cunctions of categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,7 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Category extends Model
 {
-    //
+    protected $fillable = ['title', 'slug', 'parent_id', 'published', 'created_by', 'midified_by'];
+    
+    public function setSlugAttribute($value){
+        $this->attributes['slug'] = Str::slug( mb_substr($this->title, 0, 40) . "-" . \Carbon\Carbon::now()->format('dmyHi'), "-");
+    }
+
+    // Get children category  (cheldren==дети)
+    public function children() {
+        return $this->hasMany(self::class,'parent_id');
+    }
 }
--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -17,6 +17,7 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font=awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
